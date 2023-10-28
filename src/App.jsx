94961ab7c0930a9e19ffff577225757837bf1915
stevebiczyk <stevebiczyk@gmail.com>
2023-10-28 13:35:23 +0100

    added main.scss

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import "./App.css";
 import Header from "./components/Header";
+import "./App.css";
+import "./scss/main.scss";
 
 function App() {
   const [count, setCount] = useState(0);
